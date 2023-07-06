dbec18f92a9665f61c026a53d1b01cabe4c2251a
Isaele Araújo <105069033+IsaeleAraujo@users.noreply.github.com>
2023-07-06 09:23:53 +0000

    feat: Header.jsx

--- workit-landing-react/src/scenes/Header.jsx
+++ workit-landing-react/src/scenes/Header.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import LogoLight from '../images/logo-light.svg';
 import rectangle from '../rectangle.svg';
 import CircleGroup from '../images/bg-parttern-1.svg'
+import { motion } from 'framer-motion';
+
 
 const Header = () => (
     <header className="header">
         <div className="box">
-      <div className="rectangle-wrapper">
+      
       <img src={rectangle} className="rectangle" alt="Rectangle" />
       <img src={CircleGroup} className="circle-group" alt="circle" />
-      <img src={LogoLight}  className="logo" alt="Logo" />
-      <h1 className='Data'>Data tailored to your needs.</h1>
-      <button>Learn More</button>
-      <h1>Apply for access</h1>
-      </div>
+      <motion.div
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+      >
+      <img src={LogoLight} className="logo" alt="Logo" />
+      </motion.div>
+      <h1 className='Data'>Data <span className='tailored'>tailored</span> to your needs.</h1>
+      <button className='button'>Learn more</button>
+      <h1 className='Apply'>Apply for access</h1>
+    
     </div>
 
     </header>
